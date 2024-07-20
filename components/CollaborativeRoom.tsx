303215e78296e1743213074b18100c1e0192a3bc
mohd-shibilicv <project.nasra@gmail.com>
2024-07-20 23:03:22 +0530

    feat: floating and sticky comments

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -14,8 +14,8 @@ import Loader from "./Loader";
 const CollaborativeRoom = ({
   roomId,
   roomMetadata,
+  currentUserType
 }: CollaborativeRoomProps) => {
-  const currentUserType = "editor";
   const [documentTitle, setDocumentTitle] = useState(roomMetadata.title);
   const [editing, setEditing] = useState(false);
   const [loading, setLoading] = useState(false);
@@ -122,7 +122,7 @@ const CollaborativeRoom = ({
               </SignedIn>
             </div>
           </Header>
-          <Editor />
+          <Editor roomId={roomId} currentUserType={currentUserType} />
         </div>
       </ClientSideSuspense>
     </RoomProvider>
